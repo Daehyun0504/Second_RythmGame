8831fa8d666c7b873b19207586bbf84a6636064d
Daehyun0504 <92385909+Daehyun0504@users.noreply.github.com>
2022-08-31 15:51:38 +0900

    Thrid

--- Assets/HAHAHA.cs
+++ Assets/HAHAHA.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HAHAHA : MonoBehaviour
 {
 
-    public int kimjihwan;
+    private string youngmiee;
     // Start is called before the first frame update
     void Start()
     {
